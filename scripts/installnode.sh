80a5bc9cc2bf49df4fd6374a98ef30fcf3fa35cf
Rohit Dhan <rohitdhan95@gmail.com>
2018-11-22 19:36:04 +0530

    Nvm changes

--- scripts/installnode.sh
+++ scripts/installnode.sh
@@ -1,6 +1,7 @@
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 
-. ~/.nvm/nvm.sh
+export NVM_DIR="/root/.nvm"
+[ -s "$NVM_DIR/nvm.sh"] && . "$NVM_DIR/nvm.sh" 
 
 nvm install 4.4.5
