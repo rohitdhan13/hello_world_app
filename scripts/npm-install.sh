f5d7c1a4e3b7ae05c2ca6381b0728b51d8266986
Rohit Dhan <rohitdhan95@gmail.com>
2018-11-27 00:22:03 +0530

    npm files changed

--- scripts/npm-install.sh
+++ scripts/npm-install.sh
@@ -1,5 +1,5 @@
-source /home/ec2-user/raw/.bash_profile
+source /home/ec2-user/.bash_profile
 
 cd /home/ec2-user/raw
 git clone https://github.com/rohitdhan13/hello_world_app.git
-sudo -H -u ec2-user bash -c 'npm start'
+npm install
--- scripts/npm-start.sh
+++ scripts/npm-start.sh
@@ -1,4 +1,4 @@
-source /home/ec2-user/raw/.bash_profile
+source /home/ec2-user/.bash_profile
 
 cd /home/ec2-user/raw
 sudo -H -u ec2-user bash -c 'npm start'
