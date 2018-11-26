d04843b2df883c3c2ef64b44b864524681be2d45
Rohit Dhan <rohitdhan95@gmail.com>
2018-11-26 19:43:55 +0530

    changes in the scripts file

---
+++ scripts/npm-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/ec2-user/sample-node
+git pull origin master
+npm install
---
+++ scripts/npm-start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/ec2-user/sample-node
+sudo -H -u ec2-user bash -c 'npm start'
