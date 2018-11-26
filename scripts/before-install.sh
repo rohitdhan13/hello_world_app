699e763141dc0a16e98c49cc2b81d8505a333665
Rohit Dhan <rohitdhan95@gmail.com>
2018-11-26 23:51:56 +0530

    appending the source command in script files

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+source /home/ec2-user/.bash_profile
 sudo rm -rf /home/ec2-user/raw
--- scripts/npm-install.sh
+++ scripts/npm-install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+source /home/ec2-user/.bash_profile
 
 cd /home/ec2-user/raw
 git clone https://github.com/rohitdhan13/hello_world_app.git
