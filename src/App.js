ce8a54dd46d72734d5fe8d70fbb3055067e028d0
Rohit Dhan <rohitdhan95@gmail.com>
2018-11-29 22:58:14 +0530

    change in app.js file

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Pushed the code from github through pipeline for deployment!
+            Pushed the code from github to AWS Codedeploy for deployment!
           </p>
           <a
             className="App-link"
